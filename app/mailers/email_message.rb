bd888250f32592e1469b4d55f02eafc9cb55fad8
ckib16 <ckib16@gmail.com>
2016-12-24 16:22:14 -0500

    Setup Mailgun test method

--- app/mailers/email_message.rb
+++ app/mailers/email_message.rb
@@ -12,4 +12,15 @@ class EmailMessage < ActionMailer::Base
         "#{message.author_username}: #{message.subject}"
     )
   end
+
+#   # test MailGun setup via API
+#   def send_simple_message
+#     RestClient.post "https://api:key-8c787dbf0edabecdea35c6329f57d188"\
+#     "@api.mailgun.net/v3/appfb8c8481000b4f67a19f9c7bc81119bf.mailgun.org/messages",
+#     :from => "Excited User <mailgun@appfb8c8481000b4f67a19f9c7bc81119bf.mailgun.org>",
+#     :to => "bar@example.com, pw220@sent.com",
+#     :subject => "Hello",
+#     :text => "Testing some Mailgun awesomness!"
+#   end
+
 end
